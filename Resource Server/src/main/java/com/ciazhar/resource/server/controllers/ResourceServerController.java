304663b32d44d64553d36b3d338a19fb628ceedc
ciazhar <daisetsunakama@gmail.com>
2017-02-12 11:04:35 +0700

    add controller

---
+++ Resource Server/src/main/java/com/ciazhar/resource/server/controllers/ResourceServerController.java
@@ -0,0 +1,41 @@
+package com.ciazhar.resource.server.controllers;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ResourceServerController {
+	@RequestMapping("/halo")
+    public void halo(Model m, @RequestParam(required = false) String nama){
+        m.addAttribute("waktu", new Date());
+        if(nama != null && !nama.isEmpty()){
+            m.addAttribute("pesan", "Halo "+nama);
+        }
+    }
+    @CrossOrigin
+    @RequestMapping("/api/halo")
+    @ResponseBody
+    public Map<String, Object> haloApi(@RequestBody(required = false) Map<String, String> input){
+        Map<String, Object> data = new HashMap<>();
+        data.put("waktu", new Date());
+
+        if(input != null){
+            String nama = input.get("nama");
+            if(nama != null && !nama.isEmpty()){
+                data.put("pesan", "Halo "+nama);
+            }
+        }
+
+        return data;
+    }
+}
