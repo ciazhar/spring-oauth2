3b06aed7686f0aa6c661170d134e1419b4fb87e8
ciazhar <daisetsunakama@gmail.com>
2017-02-12 10:23:18 +0700

    setup project

---
+++ Client Authorization Code/src/main/java/com/ciazhar/ClientAuthorizationCodeApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAuthorizationCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAuthorizationCodeApplication.class, args);
+	}
+}
