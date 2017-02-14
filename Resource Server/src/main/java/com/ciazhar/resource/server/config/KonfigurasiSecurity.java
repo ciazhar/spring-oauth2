8186a6469942fe0dddb94d6cf09f3d85dbc14b57
ciazhar <daisetsunakama@gmail.com>
2017-02-14 19:23:05 +0700

    memindahkan otorisasi ke database

--- Resource Server/src/main/java/com/ciazhar/resource/server/config/KonfigurasiSecurity.java
+++ Resource Server/src/main/java/com/ciazhar/resource/server/config/KonfigurasiSecurity.java
@@ -5,18 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import javax.sql.DataSource;
 
 /**
  * Created by hafidz on 12/02/17.
  */
 @Configuration
 public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private DataSource dataSource;
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+        /*auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("123")
-                .roles("USER");
+                .roles("USER");*/
+          auth.jdbcAuthentication().dataSource(dataSource)
+                  .usersByUsernameQuery("select username, password, "
+                          + "enabled from s_users where username=?")
+                  .authoritiesByUsernameQuery("select u.username, p.user_role  "
+                          + "from s_users u  "
+                          + "inner join s_permissions p on u.id = p.id_user "
+                          + "where u.username=?");
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
