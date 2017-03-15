68406795598b416490d1dabafce8858b405c05ef
ciazhar <daisetsunakama@gmail.com>
2017-03-15 16:12:07 +0700

    implicit angular js

--- Client Implicit/src/main/resources/static/js/aplikasi.js
+++ Client Implicit/src/main/resources/static/js/aplikasi.js
@@ -2,7 +2,7 @@
  * Created by ciazhar on 3/13/17.
  */
 
-var aplikasi = angular.module('OauthClientCred',[]);
+var aplikasi = angular.module('OauthClientImpl',[]);
 
 aplikasi.controller('DemoController',function ($scope, $window, $location) {
     var resourceServerUrl = "http://localhost:8080/api/halo";
@@ -12,4 +12,31 @@ aplikasi.controller('DemoController',function ($scope, $window, $location) {
         $window.location.href = authorizationServerUrl;
     };
 
+    $scope.ambilTokenDariUrl = function () {
+        var location = $location.url();
+        console.log("Location : "+location);
+        var params = location.split("&");
+        console.log("Params : "+params);
+        var tokenParam = params[0];
+        var token = tokenParam.split("=")[1];
+        console.log("Token : "+token);
+        $window.sessionStorage.setItem('token',token);
+    };
+
+    $scope.requestKeResourceServer = function () {
+        var token = $window.sessionStorage.getItem('token');
+        if (!token){
+            alert('Belum Login, silahkan klik login dulu. Kemudain klik ambil token dari url');
+            return;
+        }
+
+        $http.get(resourceServerUrl+"?access_token="+token).then(
+            function (response) {
+                $scope.responseDariServer = response.data;
+            },
+            function (response) {
+                alert('Error Code : '+response.status+', Error Text : '+response.statusText);
+            }
+        );
+    };
 });
