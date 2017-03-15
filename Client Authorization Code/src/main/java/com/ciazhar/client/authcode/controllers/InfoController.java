c95cdec43639cbfebde4628ae62fa6e3e36c30d9
ciazhar <daisetsunakama@gmail.com>
2017-03-15 21:35:49 +0700

    fix import

--- Client Authorization Code/src/main/java/com/ciazhar/client/authcode/controllers/InfoController.java
+++ Client Authorization Code/src/main/java/com/ciazhar/client/authcode/controllers/InfoController.java
@@ -5,16 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
