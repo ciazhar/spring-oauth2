603c1dc2340d89d451a4823b8603110bd6039539
ciazhar <daisetsunakama@gmail.com>
2017-03-17 16:14:40 +0700

    client user password

---
+++ Client User Password/src/main/java/com/ciazhar/ClientImplicitApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientImplicitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientImplicitApplication.class, args);
+	}
+}
---
+++ Client User Password/src/main/resources/static/js/aplikasi.js
@@ -0,0 +1,43 @@
+/**
+ * Created by ciazhar on 3/16/17.
+ */
+
+var app = angular.module('UserPassApp',[]);
+
+app.controller('DummyController',function($http, $scope, $window, $location){
+    var urlResourceServer = "http://localhost:8080/api/halo";
+    var urlAuthServer = "http://localhost:10000/oauth/authorize?client_id=jsclient&response_type=token";
+
+    $scope.bukaLoginPage = function () {
+        $window.location.href = urlAuthServer;
+    };
+
+    $scope.ambilTokenDariServer = function () {
+        var location = $location.url(); /// ngambil hash yang isinya #access_token=f2b50438-2c3a-4637-b6b9-e469543ff26d&token_type=bearer&expires_in=86399&scope=read%20write
+        console.log("Location : "+location);
+        var params = location.split("&");///jadi array yang isinya [access_token=f2b50438-2c3a-4637-b6b9-e469543ff26d , token_type=bearer , expires_in=86399 , scope=read%20write]
+        console.log("Param : "+params);
+        var tokenParam = params[0];///ambile param indek ke 0 yaitu access token
+        console.log("token Param : "+tokenParam);
+        var token = tokenParam.split("=")[1];
+        console.log("token : "+token);
+        $window.sessionStorage.setItem('token',token);
+    };
+
+    $scope.requestKeResourceServer = function () {
+
+        var token = $window.sessionStorage.getItem('token');
+        if (!token){
+            alert('Belum Login');
+            return;
+        }
+        $http.get(urlResourceServer+"?access_token="+token).then(
+            function (response) {
+                $scope.responseDariServer = response.data;
+            },
+            function (response) {
+                alert('Error Code'+response.status+', Error Text : '+response.statusText);
+            }
+        );
+    };
+});
